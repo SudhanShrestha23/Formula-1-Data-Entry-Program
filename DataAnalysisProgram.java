4030bcf9a8ab668f1bcdb4db474d598af1df82c5
SudhanShrestha23 <sudhanshrestha25@gmail.com>
2025-07-10 22:34:15 +0800

    basic functionality of data entry program finished
    need to work on try-catch loop

---
+++ DataAnalysisProgram.java
@@ -0,0 +1,6 @@
+public class DataAnalysisProgram {
+   public static void main(String[] args) {
+       System.out.println("Welcome to the FIA Formula 1 Data Analysis Program!");
+       System.out.println("Enter the name of your file");
+   } 
+}
