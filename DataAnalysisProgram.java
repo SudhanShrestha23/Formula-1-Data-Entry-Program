7252627e3fdee154c290e8b6eebcf4713c5843da
Sudhan Shrestha <sudhanshrestha25@gmail.com>
2025-08-03 16:58:27 +0800

    Separated print methhod from csv read switch case

--- DataAnalysisProgram.java
+++ DataAnalysisProgram.java
@@ -1,6 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Logger;
-
 public class DataAnalysisProgram {
    private static final Logger logger = Logger.getLogger(DataAnalysisProgram.class.getName());
    public static void main(String[] args) {
@@ -12,28 +13,30 @@ public class DataAnalysisProgram {
       do {
          System.out.println("What would you like to do?");
          System.out.println("1. Read a csv file");
-         System.out.println("2. Sort drivers based on their fastest lap times");
-         System.out.println("3. Find Teams based on car code");
-         System.out.println("4. Exit the program"); 
+         System.out.println("2. Print List of Drivers?");
+         System.out.println("3. Sort drivers based on their fastest lap times");
+         System.out.println("4. Find Teams based on car code");
+         System.out.println("5. Exit the program"); 
          int options = sc.nextInt();
          sc.nextLine();
          switch (options) {
             case 1:
                methods.readFile(sc, drivers);
-               System.out.println("Here are the contents of the file");
-               methods.printArray(drivers);
                break;
             case 2:
                methods.sortDriversByLap(drivers);
-               System.out.println("This is what the sorted list looks like");
-               methods.printArray(drivers);
+               
                break;
             case 3:
+               System.out.println("Printing Drivers");
+               methods.printArray(drivers);
+               break;
+            case 4:
                System.out.println("Enter a car code");
                String carCode = sc.nextLine();
                methods.findTeamsFromDriver(drivers, carCode);
                break;
-            case 4:
+            case 5:
                System.out.println("Exiting program");
                loop = true;
             default:
@@ -41,9 +44,5 @@ public class DataAnalysisProgram {
                break;
          }
       } while(!loop);
-      
-     
-      
-  
    } 
 }
